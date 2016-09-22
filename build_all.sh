c825111677127e243e9f9e694565d85a5a4b5c4f
Max Wittal <max.wittal@mwittal.de>
2016-09-22 03:26:33 -0700

    added build_all.sh + clean_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+mkdir -p build
 cd build
 cmake ../
+make -j8
+cd ..
+
+mkdir -p build-ReplayGUI
+cd build-ReplayGUI
+qmake CONFIG+=release -o Makefile ../ReplayGUI/ReplayGUI.pro
 make -j8
 cd ..
 
